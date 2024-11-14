0d96ec1e9d3636d3d58136d9d6bff20d127107c1
ainkay <ain.kay@kalvium.community>
2024-11-14 15:06:12 +0530

    quizapp

--- src/components/QuestionBox.jsx
+++ src/components/QuestionBox.jsx
@@ -1,15 +1,19 @@
+import { useRef } from 'react';
 import React from 'react';
 import QuestionChoice from './QuestionChoice';
 
 const QuestionBox = ({ questions, query, selectchoice }) => {
   // TASK 3: Implement logic to highlight the question text when the "Highlight" button is clicked.
   // You will need to manage state for this. Make the text red when highlighted and blue when not.
+  const ainRef=(useRef)
   const highlightHandler = () => {
     // Implement the highlight logic
+    ainRef.current.style.background =ainRef.current.style.background == 'red' ?"red":"blue"
   };
 
   const removeHighlightHandler = () => {
     // Implement the remove highlight logic
+    ainRef.current.style.color="black"
   };
 
   return (
@@ -22,7 +26,7 @@ const QuestionBox = ({ questions, query, selectchoice }) => {
           Question: {query + 1} out of {questions.length}
         </h1>
         {/* TASK 3: Change color of this text based on highlight */}
-        <h2>{questions[query].text}</h2>
+        <h2(ainRef) >{questions[query].text}</h2>
 
         <QuestionChoice
           options={questions[query].options}
